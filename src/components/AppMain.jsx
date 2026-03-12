ff07d54fc8f2ddab357e0fa55f794a50ba4c67b0
gialli24 <a.gialli2409@gmail.com>
2026-03-12 16:13:02 +0100

    Add Bootstrap and Popper.js for styling and layout enhancements

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -18,21 +18,36 @@ export default function AppMain({ articles }) {
 
     return (
         <main>
-            {
-                articlesList.map((article, index) => (
-                    <Article key={index} article={article} />
-                ))
-            }
+            <section className="articles-form border-bottom bg-body-tertiary py-4">
+                <div className="container">
+                    <form onSubmit={handleSubmit}>
+                        <h2>Inserisci un articolo</h2>
 
-            <form onSubmit={handleSubmit}>
-                <h2>Inserisci un articolo</h2>
+                        <div className="d-flex gap-4 mt-3">
+                            <input type="text" class="form-control" value={newTitle} onChange={e => { setNewTitle(e.target.value) }} />
 
-                <input type="text" value={newTitle} onChange={e => { setNewTitle(e.target.value) }} />
+                            <button type="submit" className="btn btn-primary">
+                                Aggiungi
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </section>
 
-                <button type="submit">
-                    Aggiungi
-                </button>
-            </form>
+            <section className="articles py-4">
+                <div className="container">
+
+                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+                        {
+                            articlesList.map((article, index) => (
+                                <Article key={index} article={article} />
+                            ))
+                        }
+                    </div>
+
+                </div>
+            </section>
+
         </main>
     );
 }
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,7 +2,20 @@ export default function Article({ article }) {
 
     return (
         <article>
-            <h2>{article.title}</h2>
+            <div className="pb-2 d-flex justify-content-end gap-2">
+                <button class="btn p-0 text-secondary">
+                    <i class="bi bi-pencil-square"></i>
+                </button>
+                <button class="btn p-0 text-secondary">
+                    <i class="bi bi-trash"></i>
+                </button>
+            </div>
+
+            <img src={article.image} class="rounded-4" alt={article.title}></img>
+
+            <div className="article-body py-4">
+                <h2 className="fs-4">{article.title}</h2>
+            </div>
         </article>
     )
 }
