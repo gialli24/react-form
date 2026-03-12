6d4e8c6f9c47e6108628bc625b0cf3580af94b94
gialli24 <a.gialli2409@gmail.com>
2026-03-12 15:34:36 +0100

    Refactor AppMain component to manage articles state and handle new article submissions

--- src/assets/js/articles.js
+++ src/assets/js/articles.js
@@ -1,14 +1,11 @@
 export const articles = [
     {
-        id: 1,
         title: "Article #1"
     },
     {
-        id: 2,
         title: "Article #2"
     },
     {
-        id: 3,
         title: "Article #3"
     }
 ]
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
+
 export default function AppMain({ articles }) {
+
+    const [newTitle, setNewTitle] = useState("");
+    const [articlesList, setArticlesList] = useState(articles);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const newArticle = {
+            title: newTitle
+        };
+
+        setArticlesList([newArticle, ...articlesList]);
+    }
+
     return (
         <main>
             {
-                articles.map(article => (
-                    <article key={article.id}>
+                articlesList.map((article, index) => (
+                    <article key={index}>
                         <h2>{article.title}</h2>
                     </article>
                 ))
             }
+
+            <form onSubmit={handleSubmit}>
+                <h2>Inserisci un articolo</h2>
+
+                <input type="text" value={newTitle} onChange={e => { setNewTitle(e.target.value) }} />
+
+                <button type="submit">
+                    Aggiungi
+                </button>
+            </form>
         </main>
     );
 }
